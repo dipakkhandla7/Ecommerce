e601f4d416cc078cfdc14f77057e42a2c99a6853
dipakkhandla7 <dipakkhandla7@gmail.com>
2023-05-11 14:27:30 +0530

    file

---
+++ src/NEw.js
@@ -0,0 +1,231 @@
+// const boat=[
+// 	{
+// 		id:1,
+// 		img:'/image/b131.avif',
+// 		name:'Airdopes 131',
+// 		rating:'★ 4.5',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .4274',
+// 	},
+// 	{
+// 		id:2,
+// 		img:'/image/b131t.avif',
+// 		name:' TReble Airdopes 131',
+// 		rating:'★ 4.0',
+// 		disc:'MRP to 40% Off',
+// 		price:'MRP .4574',
+// 	},
+// 	{
+// 		id:3,
+// 		img:'/image/b441.avif',
+// 		name:'Airdopes 141',
+// 		rating:'★ 4.8',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .4600',
+// 	},
+// 	{
+// 		id:4,
+// 		img:'/image/b181.avif',
+// 		name:'Airdopes 181',
+// 		rating:'★ 3.5',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .3274',
+// 	},
+// 	{
+// 		id:5,
+// 		img:'/image/b131p.avif',
+// 		name:'Airdopes 131 Pro',
+// 		rating:'★ 3.8',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .3574',
+// 	},
+// 	{
+// 		id:6,
+// 		img:'/image/b141p.avif',
+// 		name:'Airdopes 141 Pro',
+// 		rating:'★ 4.3',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .5100',
+// 	},
+// 	{
+// 		id:7,
+// 		img:'/image/b202.avif',
+// 		name:'Airdopes 202',
+// 		rating:'★ 3.6',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .4074',
+// 	},
+// 	{
+// 		id:8,
+// 		img:'/image/b402.avif',
+// 		name:'Airdopes 402',
+// 		rating:'★ 4.1',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .4274',
+// 	},
+// 	{
+// 		id:9,
+// 		img:'/image/b411anc.avif',
+// 		name:'Airdopes 411 Anc',
+// 		rating:'★ 4.8',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .5074',
+// 	},
+// 	{
+// 		id:10,
+// 		img:'/image/b141.avif',
+// 		name:'Airdopes 141',
+// 		rating:'★ 4.2',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .5874',
+// 	},
+// 	{
+// 		id:11,
+// 		img:'/image/b441.avif',
+// 		name:'Airdopes 441',
+// 		rating:'★ 4.6',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .4124',
+// 	},
+// 	{
+// 		id:12,
+// 		img:'/image/b141p.avif',
+// 		name:'Airdopes 141 pro Max',
+// 		rating:'★ 4.1',
+// 		disc:'MRP to 50% Off',
+// 		price:'MRP .5400',
+// 	},
+// ];
+
+// export default boat;
+
+
+
+
+{
+    "img": "/image/i12.jpg",
+    "rating": "★★★★",
+    "title": "iphone 12",
+    "price": "59900",
+    "quantity": 1,
+    "id": "9",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/i13m.jpg",
+    "rating": "★★★★",
+    "title": "iphone 13 Mini",
+    "price": "65900",
+    "quantity": 1,
+    "id": "10",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/i14.jpg",
+    "rating": "★★★",
+    "title": "iphone 14",
+    "price": "72990",
+    "quantity": 1,
+    "id": "11",
+    "catagory": "mobile"
+  },
+  {
+    "img": "/image/i14pro.jpg",
+    "rating": "★★★★★",
+    "title": "iphone 14 pro Max",
+    "price": "122999",
+    "quantity": 1,
+    "id": "12",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/i13m.jpg",
+    "rating": "★★★",
+    "title": "iphone 13",
+    "price": 59999",
+    "quantity": 1,
+    "id": "13",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/i14pro.jpg",
+    "rating": "★★★★★",
+    "title": "iphone 14 plus",
+    "price": "89999",
+    "quantity": 1,
+    "id": "14",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/e.jpg",
+    "rating": "★★★★★",
+    "title": "iphone 14 ",
+    "price": "65000",
+    "quantity": 1,
+    "id": "15",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/e1.jpg",
+    "rating": "★★★★★",
+    "title": "iphone 14 pro Max(256 GB)",
+    "price": "122999",
+    "quantity": 1,
+    "id": "16",
+    "catagory": "mobile"
+  },
+{
+    "img": "/image/color2.webp",
+    "rating": "★★★",
+    "title": "colorfit 2",
+    "price": "3100",
+    "quantity": 1,
+    "id": "17",
+    "catagory": "noise"
+  },
+{
+    "img": "/image/color4.webp",
+    "rating": "★★★",
+    "title": "colorfit 4",
+    "price": "6500",
+    "quantity": 1,
+    "id": "18",
+    "catagory": "noise"
+  },
+  {
+    "img": "/image/colorplus.webp",
+    "rating": "★★★",
+    "title": "colorfit plus",
+    "price": "3500",
+    "quantity": 1,
+    "id": "19",
+    "catagory": "noise"
+  },
+{
+    "img": "/image/coloricon.webp",
+    "rating": "★★★",
+    "title": "colorfit plus 2",
+    "price": "3900",
+    "quantity": 1,
+    "id": "20",
+    "catagory": "noise"
+  },
+{
+    "img": "/image/colorcaliber.webp",
+    "rating": "★★★",
+    "title": "colorfit Caliber",
+    "price": "6000",
+    "quantity": 1,
+    "id": "21",
+    "catagory": "noise"
+  },
+{
+    "img": "/image/colorloop.webp",
+    "rating": "★★★",
+    "title": "colorfit Loop",
+    "price": "5100",
+    "quantity": 1,
+    "id": "22",
+    "catagory": "noise"
+  }
+  
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <>
+      <footer id="footer">
+        <div className="container">
+          E Shoping
+          <p>Buite by | &nbsp; Dipak khandla </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
